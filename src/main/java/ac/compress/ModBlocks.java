4c691efd5a4c0b9f42055cc016b8666216b0bde9
AlexCai2019 <89138532+AlexCai2019@users.noreply.github.com>
2024-08-20 13:02:25 +0800

    新增沙子和礫石
    為了因應重力方塊而重構了方塊註冊的部分

--- src/main/java/ac/compress/ModBlocks.java
+++ src/main/java/ac/compress/ModBlocks.java
@@ -1,10 +1,14 @@
 package ac.compress;
 
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.ColoredFallingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.util.ColorCode;
 import net.minecraft.util.Identifier;
 
 import java.util.LinkedHashMap;
@@ -14,38 +18,61 @@ public class ModBlocks
 {
 	public static ModBlocks instance = new ModBlocks();
 
-	private static final String[] BLOCKS = //可被壓縮的方塊
+	private static final BlockData[] BLOCKS = //可被壓縮的方塊
 	{
-		"dirt",
-		"stone",
-		"cobblestone",
-		"cobbled_deepslate",
-		"granite",
-		"diorite",
-		"andesite",
-		"netherrack",
-		"blackstone",
-		"end_stone"
+		new BlockData("dirt", Block.class),
+		new BlockData("stone", Block.class),
+		new BlockData("cobblestone", Block.class),
+		new BlockData("cobbled_deepslate", Block.class),
+		new BlockData("granite", Block.class),
+		new BlockData("diorite", Block.class),
+		new BlockData("andesite", Block.class),
+		new BlockData("sand", ColoredFallingBlock.class),
+		new BlockData("gravel", ColoredFallingBlock.class),
+		new BlockData("netherrack", Block.class),
+		new BlockData("blackstone", Block.class),
+		new BlockData("end_stone", Block.class)
 	};
 
 	protected final Map<String, Block[]> blocksMap = new LinkedHashMap<>(); //方塊名稱與它們對應的壓縮方塊們
 
 	public void initialize()
 	{
-		for (String blockName : BLOCKS) //走訪所有要被註冊的方塊們
+		for (BlockData blockData : BLOCKS) //走訪所有要被註冊的方塊們
 		{
 			Block[] blocks = new Block[Compress.LAYERS]; //每一個陣列元素 都是一個壓縮方塊
 
 			for (int layer = 0; layer < Compress.LAYERS; layer++) //以層數作為名稱的流水號
 			{
-				blocks[layer] = new Block(Registries.BLOCK.get(Identifier.of(Identifier.DEFAULT_NAMESPACE, blockName)).getSettings()); //建立一個新方塊 使用原本的屬性
+				blocks[layer] = createBlock(blockData); //註冊方塊
 
-				Identifier id = Identifier.of(Compress.MOD_ID, blockName + '_' + (layer + 1)); //新方塊的ID
+				Identifier id = Identifier.of(Compress.MOD_ID, blockData.name + '_' + (layer + 1)); //新方塊的ID
 				Registry.register(Registries.BLOCK, id, blocks[layer]); //註冊方塊
 				Registry.register(Registries.ITEM, id, new BlockItem(blocks[layer], new Item.Settings())); //註冊對應的物品
 			}
 
-			blocksMap.put(blockName, blocks); //方塊放入map裡
+			blocksMap.put(blockData.name, blocks); //方塊放入map裡
 		}
 	}
+
+	private Block createBlock(BlockData blockData)
+	{
+		AbstractBlock.Settings settings = Registries.BLOCK.get(Identifier.of(Identifier.DEFAULT_NAMESPACE, blockData.name)).getSettings();
+
+		//建立一個新方塊 使用原本的屬性
+		if (blockData.type == Block.class)
+			return new Block(settings);
+
+		if (blockData.type == ColoredFallingBlock.class)
+			return new ColoredFallingBlock(new ColorCode(switch (blockData.name)
+			{
+				case "sand" -> 0xDBD3A0;
+				case "gravel" -> 0xFF807C7B;
+				default -> 0;
+			}), settings);
+
+		return Blocks.AIR;
+	}
+
+	private record BlockData(String name, Class<? extends Block> type) {}
 }
